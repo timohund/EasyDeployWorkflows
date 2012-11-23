4f277b8272c4a78691fee4ae6f05ef645e6841fb
TImo Schmidt <timo@timo-UL30A.(none)>
2012-11-23 13:59:43 +0100

    Added  SolrConfigurationTest

---
+++ Tests/Mocked/Workflows/Solr/SolrConfigurationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use EasyDeployWorkflows\Workflows\Solr as Solr;
+
+require_once EASYDEPLOY_WORKFLOW_ROOT . 'Classes/Workflows/AbstractConfiguration.php';
+require_once EASYDEPLOY_WORKFLOW_ROOT . 'Classes/Workflows/AbstractWorkflowConfiguration.php';
+require_once EASYDEPLOY_WORKFLOW_ROOT . 'Classes/Workflows/Solr/SolrConfiguration.php';
+
+class SolrConfigurationTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var $configuration Solr\SolrConfiguration
+	 */
+	protected $configuration;
+
+	/**
+	 * @return void
+	 */
+	public function setUp() {
+		$this->configuration = new Solr\SolrConfiguration();
+	}
+
+	/**
+	 * @test
+	 */
+	public function setInstancePath() {
+		$this->configuration->setInstancePath('/opt/solr');
+		$this->assertEquals('/opt/solr', $this->configuration->getInstancePath(),'Unexpected instance path');
+	}
+
+	/**
+	 * @test
+	 */
+	public function setRestartCommand() {
+		$this->assertEquals($this->configuration->getRestartCommand(),'','unexpected default restart script');
+		$this->configuration->setRestartCommand('/etc/init.d/solr restart');
+		$this->assertEquals('/etc/init.d/solr restart',$this->configuration->getRestartCommand(),'unexpected restart command');
+	}
+}
